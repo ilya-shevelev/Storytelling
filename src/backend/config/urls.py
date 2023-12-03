835f550ffd230bb9c452db685586c992963a8bda
Ilya Shevelev <ilasevelev10776@gmail.com>
2023-12-03 18:30:56 +0300

    Docs

--- src/backend/config/urls.py
+++ src/backend/config/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
