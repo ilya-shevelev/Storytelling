37a4662d9757a3733bceaf647c5e07982b9cf4eb
Ilya Shevelev <ilasevelev10776@gmail.com>
2023-12-03 13:05:13 +0300

    Initial commit

---
+++ src/backend/docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+if [[ "$DJANGO_ENV" = "prod" ]]; then
+  gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+else
+  python manage.py initadmin
+  python manage.py runserver 0.0.0.0:8000
+fi
